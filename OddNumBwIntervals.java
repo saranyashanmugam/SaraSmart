3f60dd8ffdc0bfb96ea52a8e76a6dace3977b30b
saranyashanmugam <saranya.scse@ksrce.ac.in>
2016-07-22 17:50:05 +0530

    Create OddNumBwIntervals.java

---
+++ OddNumBwIntervals.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class OddNumBwIntervals {
+
+	public static void main(String[] args) {
+		Scanner scr=new Scanner(System.in);
+		System.out.println("Enter the Range");
+		int n1=scr.nextInt();
+		int n2=scr.nextInt();
+		while(n1<=n2){
+			if(n1%2==0){
+				System.out.print(n1+" ");
+			}
+			n1++;
+		}
+
+	}
+
+}
