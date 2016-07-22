5a7aed650eec5e14e8bc705f43598eabcb3cbde2
saranyashanmugam <saranya.scse@ksrce.ac.in>
2016-07-22 16:54:22 +0530

    Create NumberOfDigits.java

---
+++ NumberOfDigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class NumberOfDigits {
+
+	
+	public static void main(String[] args) {
+		Scanner scr=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int number=scr.nextInt();
+		int count=0;
+		count=String.valueOf(number).length();
+        System.out.println("The number of digits in a given  number "+count);
+        scr.close();
+	}
+
+}
