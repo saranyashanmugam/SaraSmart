17ec9f30de33402e0d3b67862d482f3d68133dfc
saranyashanmugam <saranya.scse@ksrce.ac.in>
2016-08-10 18:06:00 +0530

    Create ArrayConcatenation.java

---
+++ ArrayConcatenation.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayConcatenation {
+ public static void main(String []args){
+	 Scanner scr=new Scanner(System.in);
+	 String x="";
+	 System.out.println("Enter the size of array");
+	 int n=scr.nextInt();
+	 int arr[]=new int[n];
+	 for(int i=0;i<n;i++){
+		 arr[i]=scr.nextInt();
+	 }
+	 Arrays.sort(arr);
+	 for(int j=arr.length-1;j>=0;j--){
+		x+=arr[j];
+	 }
+	 System.out.println(x);
+ }
+}
